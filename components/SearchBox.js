65394f4f44618555ef155d7668b956f7426d3b83
sriram <sriramhegdeofficial@gmail.com>
2020-11-22 16:48:10 +0530

    fdsafsd

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    searchIconStyle: {
+        color: 'rgba(235,87,87,0.9)',
+        width: '25px',
+        height: '25px',
+       
+        
+    }
+  });
+
 
 const SearchBox = (props) => {
+    const classes = useStyles();
     return (
         <React.Fragment>
             <div className="search__main__wrapper">
                 <div className="search__wrapper">
                     <div className="search__location__wrapper"></div> 
                     <div className="search__guest__wrapper"></div>
-                    <div className="search__icon__wrapper"></div>
+                    <div className="search__icon__wrapper">
+                            <SearchIcon className={classes.searchIconStyle} />
+                    </div>
                 </div>
             </div>
             <style jsx>
@@ -15,7 +32,6 @@ const SearchBox = (props) => {
                     `
                     .search__main__wrapper {
                         width: 100%;
-                        border: 1px solid black;
                         padding: 32px 15px; 
                         display: flex;
                         justify-content: center;
@@ -28,7 +44,6 @@ const SearchBox = (props) => {
                         box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
                         border-radius: 16px;
                         height: 65px;
-                        border: 1px solid black;
                         overflow: hidden;
                         display: flex;
                     }
@@ -36,20 +51,31 @@ const SearchBox = (props) => {
                     .search__location__wrapper {
                         flex: 1 1 auto;
                         height: 100%;
-                        border: 2px solid blue;
+                        border-right: 1px solid rgba(242,242,242,1);
+                        
                     }
 
                     .search__guest__wrapper {
                         flex: 1 1 auto;
                         height: 100%;
-                        border: 2px solid green;
+                        border-right: 1px solid rgba(242,242,242,1);
+                        
                     }
 
                     .search__icon__wrapper {
-                        min-width: 70px;
-                        width: 70px;
+                        min-width: 55px;
+                        width: 55px;
                         height: 100%;
-                        border: 2px solid black;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+
+                    .search__icon {
+                        width: 17px;
+                        height: 17px;
+                        color: #EB5757;
+
                     }
                     
                     `
